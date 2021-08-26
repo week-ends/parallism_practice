c150c45f2d28b5774efae49e68c360764c3ff7c9
mooniljoo <ezekielmoon@naver.com>
2021-08-26 15:20:48 +0900

    Update worker.py

--- worker.py
+++ worker.py
@@ -3,23 +3,23 @@ from celery.signals import worker_init
 from celery.signals import worker_shutdown
 from celery.bin.celery import result
 
-//rabbitmq를 설정해준다
-//celery는 default로 rabbitmq를 브로커(messagequeue)로 사용한다.
-//guest는 rabbitmq에서 지원해주는 가장 default 큐다.
+# rabbitmq를 설정해준다
+# celery는 default로 rabbitmq를 브로커(messagequeue)로 사용한다.
+# guest는 rabbitmq에서 지원해주는 가장 default 큐다.
 app = Celery('tasks', broker='amqp://guest:guest@localhost:5672//')
 
-//워커가 초기화됬을때 
+# When Worker initialize
 @worker_init.connect
 def init_worker(**kwargs):
 	print('init')
 
-//워커가 종료됬을때
+# When Worker shutdown
 @worker_shutdown.connect
 def shutdown_worker(**kwargs):
 	print('shut')
 
-//실제 task들을 받는 부분.
-//받은데이터를 thread로 처리한다.
+# 실제 task들을 받는 부분.
+# Handling data with thread
 @app.task(name='task')
 def worker(data):
 	Thread = threading.Thread(target=run, args=(data,))
